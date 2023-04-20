cc65749a0977c8cb32a6466a67e17d741446324f
renatohvo <renatohvo@gmail.com>
2023-04-20 19:13:24 -0300

    Delete User(DELETE)

--- backend/src/main/java/com/renatohvo/rhvopostmongodb4/controllers/UserController.java
+++ backend/src/main/java/com/renatohvo/rhvopostmongodb4/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,5 +51,11 @@ public class UserController {
 	public ResponseEntity<UserDTO> update(@PathVariable String id, @RequestBody UserDTO obj) {
 		obj = service.update(id, obj);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- backend/src/main/java/com/renatohvo/rhvopostmongodb4/services/UserService.java
+++ backend/src/main/java/com/renatohvo/rhvopostmongodb4/services/UserService.java
@@ -46,6 +46,11 @@ public class UserService {
 		entity = repository.save(entity);
 		return new UserDTO(entity);
 	}
+	
+	public void delete(String id) {
+		getEntityById(id);
+		repository.deleteById(id);
+	}
 
 	private void copyDtoToEntity(UserDTO dto, User entity) {
 		entity.setName(dto.getName());
