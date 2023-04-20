9f4e6bcaac144e967e23e2285e3c7224bc33d091
renatohvo <renatohvo@gmail.com>
2023-04-20 19:25:32 -0300

    Exception Handling

---
+++ backend/src/main/java/com/renatohvo/rhvopostmongodb4/controllers/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.renatohvo.rhvopostmongodb4.controllers.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.renatohvo.rhvopostmongodb4.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		
+		StandardError error = new StandardError();
+		error.setError("Not Found");
+		error.setMessage(e.getMessage());
+		error.setPath(request.getRequestURI());
+		error.setStatus(status.value());
+		error.setTimestamp(Instant.now());
+	
+		return ResponseEntity.status(status).body(error);
+	}
+}
