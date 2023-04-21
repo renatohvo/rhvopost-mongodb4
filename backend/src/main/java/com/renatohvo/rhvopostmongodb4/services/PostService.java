a83c8fe77be112f99683f66216920d298dd15135
renatohvo <renatohvo@gmail.com>
2023-04-21 10:29:28 -0300

    Imports-PostService

--- backend/src/main/java/com/renatohvo/rhvopostmongodb4/services/PostService.java
+++ backend/src/main/java/com/renatohvo/rhvopostmongodb4/services/PostService.java
@@ -1,8 +1,6 @@
 package com.renatohvo.rhvopostmongodb4.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
